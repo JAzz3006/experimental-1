5d7d71466a91bec356052cd5b082bbf94e203ab6
Grigoriy Chernykh <che90@mail.ru>
2024-06-27 08:46:41 +0500

    Main class added

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static final int LOWEST_VALUE = 0;
+    public static final int HIGHEST_VALUE = 10;
+
+    public static void main(String[] args) {
+        System.out.println("Я загадала число в диапазоне от "
+                + LOWEST_VALUE + " до " + HIGHEST_VALUE +
+                "\nПопробуйте угадать.\n");
+
+    }
+}
